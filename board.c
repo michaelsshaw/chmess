f0f57e5c4245bb16696379dd3e13b0c691009c68
Michael Shaw <spencershawiscool@gmail.com>
2023-10-11 14:23:20 -0400

    change STARTING_FEN back to actual start position

--- board.c
+++ board.c
@@ -6,7 +6,7 @@
 	struct piece_mapping map_##_fen = { .id = _id }; \
 	mappings[(int)#_fen[0]] = map_##_fen
 
-#define STARTING_FEN "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
+#define STARTING_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 struct piece_mapping {
 	char id;
